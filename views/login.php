14d01239a3e209360ea775d57a86fed715510875
Ahmetcan İrdem <cargamoni@localhost.localdomain>
2017-09-27 03:02:32 +0300

    Login Html Fix

--- views/login.php
+++ views/login.php
@@ -1,17 +1,3 @@
-<!DOCTYPE html>   <!--footer dakapanacak-->
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-        <title>Video Life</title>
-        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
-        <script src="https://use.fontawesome.com/2fdf45b284.js"></script>
-
-    </head>
-<!--    kullanıcı email, kullanıcı adı, password ile sisteme giriş yapacak. formun nasıl görünecegi burada-->
-    <body>
-        <!--oturum açma butonu işlemleri için gerekli olan kısım kayıt olma ve oturum açma hesabı-->
         <div >
             <ul class="nav navbar-nav navbar-right">
                 <!--                                        kayıt paneli-->
@@ -89,6 +75,3 @@
 
             </ul>
         </div>
-
-    </body>
-</html>
