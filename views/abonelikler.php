09fa25ab9d7b7cdb85d51f8fa9935581f98717ce
aquamarin <cicekliebru@gmail.com>
2017-10-14 05:19:39 +0300

    some changes

--- views/abonelikler.php
+++ views/abonelikler.php
@@ -1,10 +1,46 @@
 <?
 require_once "header.php";
-
 ?>
 <div class="content" style="margin-top: 5px;">
   <? require_once "top_menu.php"; ?>
-  <div class="row">
-    <? require_once "left-dropdown.php"; ?>
-  </div>
+  <? require_once "left-dropdown.php"; ?>
+  <div class="col-lg-10">
+    <p class="Text">Abone Olduğunuz Kanallarda Paylaşılan Videolar</p>
+    <div class="thisWeek">
+      <h3>Bu Hafta</h3>
+      <div class="row">
+        <? for ($i=0; $i <6 ; $i++) : ?>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 videoFix">
+            <a href="#">
+              <img src="/Video-Life-v2/images/Chandelier - Sia _ Lia Kim Choreography.jpg" title="Sia _ Lia Kim Choreography" class="image img-thumbnail">
+              <div class="time">
+                <time>2:45</time>
+              </div>
+              <div class="videoText">
+                <p>Chandelier - Sia _ Lia Kim Choreography</p>
+              </div>
+            </a>
+          </div>
+        <? endfor; ?>
+      </div> <!-- ./row -->
+    </div>   <!-- ./thisWeek -->
+    <div class="thisMonth">
+      <h3>Bu Ay</h3>
+      <div class="row">
+        <? for ($i=0; $i <6 ; $i++) : ?>
+          <div class="col-xs-12 col-sm-6 col-md-4 col-lg-2 videoFix">
+            <a href="#">
+              <img src="/Video-Life-v2/images/Chandelier - Sia _ Lia Kim Choreography.jpg" title="Sia _ Lia Kim Choreography" class="image img-thumbnail">
+              <div class="time">
+                <time>2:45</time>
+              </div>
+              <div class="videoText">
+                <p>Chandelier - Sia _ Lia Kim Choreography</p>
+              </div>
+            </a>
+          </div>
+        <? endfor;  ?>
+      </div> <!-- ./row -->
+    </div>   <!-- ./thisMouth -->
+  </div>  <!-- col-lg-10 -->
 </div>
--- views/videoPage.php
+++ views/videoPage.php
@@ -3,31 +3,37 @@ require_once "header.php";
 
 ?>
 <div class="content">
-    <? require_once "top_menu.php"; ?>
-    <div class="row">
-        <? require_once "left-dropdown.php"; ?>
-        <div id="videoPage">
-            <video class="col-sm-12 col-lg-6" controls>
-                <source src="/Video-Life-v2/videos/BLACKPINK-(BOOMBAYAH) M_V.mp4" type="video/mp4">
-            </video>
+  <? require_once "top_menu.php"; ?>
+  <? require_once "left-dropdown.php"; ?>
+  <div id="videoPage col-lg-10">
+    <div class="col-sm-12 col-md-6 col-lg-6">
+      <video class="col-sm-12 col-md-12 col-lg-12" controls>
+        <source src="/Video-Life-v2/videos/BLACKPINK-(BOOMBAYAH) M_V.mp4" type="video/mp4">
+      </video>
+      <div class="videoDetails">
+        <p class="videoName">BLACKPINK-(BOOMBAYAH) M_V</p>
+        <div class="ex3">
+          <i class="fa fa-facebook fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-twitter fa-2x" aria-hidden="true"></i>
+          <i class="fa fa-instagram fa-2x" aria-hidden="true"></i>
         </div>
-        <? for ($i=0; $i<4; $i++): ?>
-        <div class="col-sm-12 col-lg-2" >
-            <div id="videoList">
-                <div>
-                    <a href="">
-                        <img src="/Video-Life-v2/images/TWICE(트와이스) _TT_ ♥ dance cover.jpg" title="TWICE-TT" class="img-thumbnail" id="thumbnail">
-                    </a>
-                </div>
-                <div class="media-body">
-                    <p>TWICE(트와이스) _TT_ ♥ dance cover</p>
-                </div>
-
-
-            </div>
-        </div>
-        <?endfor;?>
+      </div>
     </div>
-
-
-</div>
+    <div class=" videoList">
+      <? for ($i=0; $i<4; $i++): ?>
+      <div class="col-sm-12 col-md-3 col-lg-2" >
+        <div >
+          <div>
+            <a href="">
+              <img src="/Video-Life-v2/images/TWICE(트와이스) _TT_ ♥ dance cover.jpg" title="TWICE-TT" class="img-thumbnail" id="thumbnail">
+            </a>
+          </div>
+          <div class="media-body">
+            <p>TWICE(트와이스) _TT_ ♥ dance cover</p>
+          </div>
+        </div>      <!-- ./videoList  -->
+      </div>      <!-- ./col-sm-12 col-lg-2 -->
+      <?endfor;?>
+    </div>
+  </div>      <!--./videoPage ./col-lg-10  -->
+</div>      <!-- ./content -->
